4fdf362b840512061df86e778c34533ee42d0500
LittleSec <517862788@qq.com>
2020-11-02 11:01:15 +0800

    weibojpg.sh: add parallel download
    fixme: succ_cnt and fail_cnt not right

--- script/weibojpg.sh
+++ script/weibojpg.sh
@@ -15,6 +15,25 @@ else
   exit 127
 fi
 
+# parallel download
+OS_TYPE=$(uname)
+if [ $OS_TYPE = "Linux" ]; then
+  Nproc=$(nproc)
+elif [ $OS_TYPE = "Darwin" ]; then # macOS
+  Nproc=$(sysctl -n hw.logicalcpu)
+else
+  echo "[\!] Can handle this OS type, default Nproc=4"
+  Nproc=4
+fi
+
+Pfifo="./$$.fifo"
+mkfifo Pfifo
+exec 4<> Pfifo
+rm -rf Pfifo
+for i in `seq 1 $Nproc`; do
+  echo >&4
+done
+
 # SRC_URL=https://wx1.sinaimg.cn/large
 SRC_URL=https://wx2.sinaimg.cn/large
 # SRC_URL=https://wx3.sinaimg.cn/large
@@ -24,17 +43,25 @@ succ_cnt=0
 fail_cnt=0
 
 for fjpg in `ls $1`; do
-  cmd="$WGET_PATH $SRC_URL/$fjpg -o /dev/null"
-  # echo $cmd
-  $cmd
-  if [ 0 -ne $? ]; then
-    echo "[-] fail: $fjpg"
-    let fail_cnt+=1
-  else
-    echo "[+] succ: $fjpg"
-    let succ_cnt+=1
-  fi
+  read -u 4 # get token
+  {
+    cmd="$WGET_PATH $SRC_URL/$fjpg -o /dev/null"
+    # echo $cmd
+    $cmd
+    if [ 0 -ne $? ]; then
+      echo "[-] fail: $fjpg"
+      let fail_cnt+=1
+    else
+      echo "[+] succ: $fjpg"
+      let succ_cnt+=1
+    fi
+    echo >&4 # set toker
+  } &
 done
+
+wait
+exec 4>&-
+exec 4<&-
 
 echo "===== Summary ====="
 echo "   succ: $succ_cnt"
