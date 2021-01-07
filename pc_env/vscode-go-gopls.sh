0228e68c33b62c493074c2084c6279d7298e89ac
LittleSec <517862788@qq.com>
2021-01-07 22:40:12 +0800

    add vscode-go-gopls.sh to support gopls

---
+++ pc_env/vscode-go-gopls.sh
@@ -0,0 +1,40 @@
+# go version > 1.13(because gopls->xurls)
+
+gopls_deps = (
+    golang.org/x/tools,
+    golang.org/x/mod,
+    golang.org/x/sync,
+    github.com/sergi/go-diff,
+    honnef.co/go/tools,
+    mvdan.cc/gofumpt,
+    mvdan.cc/xurls
+)
+
+git clone https://github.com/golang/tools.git golang.org/x/tools --depth=1
+git clone https://github.com/golang/mod.git golang.org/x/mod --depth=1
+
+git clone https://github.com/sergi/go-diff.git github.com/sergi/go-diff --depth=1
+go install github.com/sergi/go-diff/diffmatchpatch
+
+git clone https://github.com/golang/sync.git golang.org/x/sync --depth=1
+go install golang.org/x/sync/errgroup
+
+git clone https://github.com/dominikh/go-tools.git honnef.co/go/tools --depth=1
+go install honnef.co/go/tools/simple
+git clone https://github.com/BurntSushi/toml.git github.com/BurntSushi/toml --depth=1
+go install github.com/BurntSushi/toml
+go install golang.org/x/tools/go/analysis
+go install honnef.co/go/tools/stylecheck
+go install honnef.co/go/tools/staticcheck # dependency: (1)github.com/BurntSushi/toml
+
+git clone https://github.com/mvdan/gofumpt.git mvdan.cc/gofumpt --depth=1
+git clone https://github.com/google/go-cmp.git github.com/google/go-cmp --depth=1
+go install github.com/google/go-cmp/cmp
+go install mvdan.cc/gofumpt/format # dependency: (1)github.com/google/go-cmp/cmp
+
+git clone https://github.com/mvdan/xurls.git mvdan.cc/xurls --depth=1
+# git clone https://github.com/mvdan/xurls.git mvdan.cc/xurls/v2 --depth=1
+go install mvdan.cc/xurls
+# go install mvdan.cc/xurls/v2
+
+go install golang.org/x/tools/gopls
